46ffc126692bf3038ec169c0f4e83df141ed1e6a
mich71095 <michaelpsy71095@gmail.com>
2018-01-31 20:10:58 +0800

    invoices urls.py fixes

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
-from invoices.views import GeneratePdf,PDF,InvoiceEmailView,MakeInvoiceView,IndexView,InvoicesView,InvoiceViewView,InvoiceAddView, InvoiceEditView,InvoiceDeleteView,InvoicePdfView
+from invoices import views
 
 
-
-in_id = '<int:invoice_id>'
 
 urlpatterns = [
-	path('', IndexView.as_view(), name='index'),
-	path('invoices/'               , InvoicesView.as_view(),     name='invoices'      ),
-	path('invoice/view/'+in_id+'/'  , InvoiceViewView.as_view(),  name='invoice_view'  ),
-	path('invoice/add/'            , InvoiceAddView.as_view(),   name='invoice_add'   ),
-	path('invoice/edit/'+in_id+'/'  , InvoiceEditView.as_view(),  name='invoice_edit'  ),
-	path('invoice/delete/'+in_id+'/', InvoiceDeleteView.as_view(),name='invoice_delete'),
-	path('invoice/pdf/<int:user_id>/', GeneratePdf.as_view(),   name='invoice_pdf'   ),
-	path('clients/make_invoice/<int:client_id>/'  , MakeInvoiceView.as_view(),   name='make_invoice'   ),
-	path('invoice/send_email/'+in_id+'/', InvoiceEmailView.as_view(), name='email'),
-	path('pdf', PDF.as_view(),name='pdf')
+    path('', views.IndexView.as_view(), name='index'),
+    path('invoices/', views.InvoicesView.as_view(), name='invoices'),
+    path('invoice/view/<int:invoice_id>/', views.InvoiceViewView.as_view(), name='invoice_view'),
+    path('invoice/add/', views.InvoiceAddView.as_view(), name='invoice_add'),
+    path('invoice/edit/<int:invoice_id>/', views.InvoiceEditView.as_view(), name='invoice_edit'),
+    path('invoice/delete/<int:invoice_id>/', views.InvoiceDeleteView.as_view(), name='invoice_delete'),
+    path('invoice/send_email/<int:invoice_id>/', views.InvoiceEmailView.as_view(), name='email'),
+
+    path('clients/make_invoice/<int:client_id>/', views.MakeInvoiceView.as_view(), name='make_invoice'),
 ]
