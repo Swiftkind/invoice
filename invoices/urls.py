52dba1fe3ec4b17673513d9fdb543bf5637442af
mich71095 <michaelpsy71095@gmail.com>
2018-01-31 22:34:09 +0800

    Views fixes

--- invoices/urls.py
+++ invoices/urls.py
@@ -5,8 +5,8 @@ from invoices import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('invoices/', views.InvoicesView.as_view(), name='invoices'),
-    path('invoice/view/<int:invoice_id>/', views.InvoiceViewView.as_view(), name='invoice_view'),
+    path('invoices/', views.InvoiceListView.as_view(), name='invoices'),
+    path('invoice/view/<int:invoice_id>/', views.InvoiceView.as_view(), name='invoice_view'),
     path('invoice/add/', views.InvoiceAddView.as_view(), name='invoice_add'),
     path('invoice/edit/<int:invoice_id>/', views.InvoiceEditView.as_view(), name='invoice_edit'),
     path('invoice/delete/<int:invoice_id>/', views.InvoiceDeleteView.as_view(), name='invoice_delete'),
