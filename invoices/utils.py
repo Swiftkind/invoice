73ab4773859d217f3caddc59eea66fe0e52b9180
mich71095 <michaelpsy71095@gmail.com>
2018-01-31 21:33:56 +0800

    utils.py fixes

--- invoices/utils.py
+++ invoices/utils.py
@@ -5,17 +5,17 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 def render_to_pdf(template_src, context_dict={}):
-	template = get_template(template_src)
-	html  = template.render(context_dict)
-	result = BytesIO()
-	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-	if not pdf.err:
-		return HttpResponse(result.getvalue(), content_type='application/pdf')
-	return None
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
 
 
 
 def get_invoice_directory(self, filename):
-	return 'invoices/{id}/{pdf}'.format(id=self.id, pdf=filename)
+    return 'invoices/{id}/{pdf}'.format(id=self.id, pdf=filename)
 
 
