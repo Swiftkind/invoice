f1c38b95ea18003ea70a34adedd9e155cca48b5b
mich71095 <michaelpsy71095@gmail.com>
2018-02-06 15:34:23 +0800

    User password reset urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+
 from users import views
+
+
 
 urlpatterns = [
     path('', views.SignupView.as_view(), name='index'),
@@ -8,4 +13,11 @@ urlpatterns = [
     path('signout/', views.SignoutView.as_view(), name='signout'),
     path('user/profile/<int:user_id>/', views.UserProfileView.as_view(), name='profile'),
     path('user/update/<int:user_id>/', views.UserUpdateView.as_view(), name='user_update'),
+
+    # Reset Password
+    path('password_reset/', auth_views.password_reset, name='password_reset'),
+    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+    path('reset/<str:uidb64>/<slug:token>/',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
    ]
