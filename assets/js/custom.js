f48fd96ce642d5d7ea033b2a9d432fdd8816b925
mich71095 <michaelpsy71095@gmail.com>
2018-02-16 09:26:56 +0800

    Amount computation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,62 +1,27 @@
-$(document).ready(function(){
-
-	/*client*/
 
 
-	$('input#id_first_name').keyup(function(){
-		var f = $(this).val();
-		var l = $('input#id_last_name').val()
-		$('input#id_display_name').val(f+' '+l);
-	});
+  $(document).ready(function() {
+    $('input#id_first_name').keyup(function(){
+      var f = $(this).val();
+      var l = $('input#id_last_name').val()
+      $('input#id_display_name').val(f+' '+l);
+    });
 
-	$('input#id_last_name').keyup(function(){
-		var l = $(this).val();
-		var f = $('input#id_first_name').val()
-		$('input#id_display_name').val(f+' '+l);
-	});
+    $('input#id_last_name').keyup(function(){
+      var l = $(this).val();
+      var f = $('input#id_first_name').val()
+      $('input#id_display_name').val(f+' '+l);
+    });
 
+    if ($( "select#id_item_type").val() == 'fixed'){
+      $('input#id_amount').removeAttr("disabled")
+    }else if($( "select#id_item_type").val() == 'quantity'){
+      $('input#id_quantity').removeAttr("disabled")
+      $('input#id_rate').removeAttr("disabled")
+    }
 
-	if ($( "select#id_item_type").val() == 'fixed'){
-		$('input#id_amount').removeAttr("disabled")
-	}else if($( "select#id_item_type").val() == 'quantity'){
-		$('input#id_quantity').removeAttr("disabled")
-		$('input#id_rate').removeAttr("disabled")
-
-	}
-
-
-	
-
-	$( "select#id_item_type").click(function(){
-		var selected = $(this).val();
-
-		if (selected == 'quantity'){
-			
-			$('input#id_quantity').removeAttr("disabled")
-			$('input#id_rate').removeAttr("disabled")
-			$('input#id_amount').attr("disabled", "true")
-
-		}
-		if(selected == 'fixed'){
-
-			$('input#id_quantity').attr("disabled", "true")
-			$('input#id_rate').attr("disabled", "true")
-			$('input#id_amount').removeAttr("disabled")
-		}
-	}); 
-
-
-
-$( "select#items").click(function(){
-		var selected = $(this).val();
-
-		$('div#item-pick').append('<span>'+selected+'</span>')
-
-});
-
-
-
-
-
-
-});
+    $( "select#items").click(function(){
+      var selected = $(this).val();
+      $('div#item-pick').append('<span>'+selected+'</span>')
+    });
+  });
