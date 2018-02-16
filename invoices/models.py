eb0d6e453a5081b82fc5d3f1026fffd3712a8c8b
mich71095 <michaelpsy71095@gmail.com>
2018-02-16 09:35:10 +0800

    Add item model and change invoices field

--- invoices/models.py
+++ invoices/models.py
@@ -1,16 +1,15 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
-from items.models import Item
+
 from clients.models import Client
 from users.models import User, Company
 from invoices.utils import get_invoice_directory
 
 
-
 class Invoice(models.Model):
-    '''creating database for invoice
-    '''
+    """ Creating database for invoice
+    """
     DRAFT = 'draft'
     SENT = 'sent'
     
@@ -18,28 +17,27 @@ class Invoice(models.Model):
         (DRAFT, 'Draft'),
         (SENT, 'Sent'),
     )
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='invoice', default='')
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    archive = models.BooleanField(default=False)
     client  = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)
-    
-    invoice_number = models.PositiveIntegerField()
-    invoice_date = models.DateField()
-    due_date = models.DateField()
-    item = models.ForeignKey(Item ,on_delete=models.SET_NULL, null=True) 
-    
-    status = models.CharField(max_length=10, choices=STATUS, default='draft')
-    paid = models.BooleanField(default=False)
-    remarks = models.TextField(max_length=255,null=True, blank=True)
-   
-    pdf = models.FileField(upload_to=get_invoice_directory,null=True, blank=True)
-    description = models.CharField(max_length=255, null=True, blank=True)
-
-    payment_status = models.BooleanField( default=False)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    due_date = models.DateField()
+    invoice_number = models.PositiveIntegerField()
+    invoice_date = models.DateField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE, 
+                              related_name='invoice', 
+                              default=''
+                              )
+    payment_status = models.BooleanField( default=False)
+    pdf = models.FileField(upload_to=get_invoice_directory,null=True, blank=True)
+    remarks = models.TextField(max_length=255,null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='draft')
 
     class Meta:
-        unique_together = (("invoice_number", "company"),)
+        unique_together = ('invoice_number', 'company')
 
     def __str__(self):
         return f"{self.invoice_number}"
@@ -48,8 +46,23 @@ class Invoice(models.Model):
         return f"{self.invoice_number}".zfill(9)
 
 
+class Item(models.Model):
+    """ Item form
+    """
+    amount = models.PositiveIntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE, 
+                              related_name='items', 
+                              default=''
+                              )
+    quantity = models.PositiveIntegerField(null=True, blank=True)
+    rate = models.PositiveIntegerField(null=True, blank=True)
 
-
-
+    def __str__(self):
+        return f"{self.description}"
 
 
