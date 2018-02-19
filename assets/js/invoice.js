805f9d180bc10753d8dbd7df1ee157ce97da44f6
mich71095 <michaelpsy71095@gmail.com>
2018-02-20 06:09:15 +0800

    Delete amount computation, change strict mode and change var to let

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -1,41 +1,28 @@
+'use strict';
+$(function() {
 
-  $(document).ready(function() {
-
-    // 
+    // Display invoice form
     $("a.create-invoice").on('click', function(e) {
       e.preventDefault();
       $("div.invoice-box").empty();
       $("div.invoice-box-form").show();
     });
 
-    // Total amount calculation
-    $('.field-quantity').on('keydown keyup keypress focusout focus', function() {
-        var rate = $(this).parent().children('.field-rate').children().val();
-        var quantity = $(this).children().val();
-        var total = parseInt(rate) + parseInt(quantity);
-        $(this).parent().children('.field-amount').children().val(total)
-    });
-    $('.field-rate').on('keydown keyup keypress focusout focus', function() {
-        var rate = $(this).children().val();
-        var quantity = $(this).parent().children('.field-quantity').children().val();
-        var total = parseInt(rate) + parseInt(quantity);
-        $(this).parent().children('.field-amount').children().val(total)
-    });
-
-
     // Ajax view invoice
     $("div.invoice-data").on('click', function(e) {
       e.preventDefault();
-      var id = $(this).attr('id').toString();
+      let id = $(this).attr('id').toString();
+
       $.ajax({
        method: 'GET',
        url: "/invoice/ajax/view/"+id+"",
        context: $(this),
-     })
+      })
       .done(function(response){
-        var obj = JSON.parse(response.invoice);
+        let obj = JSON.parse(response.invoice);
 
-        for (var data in obj) {
+        // Create invoice details template
+        for (let data in obj) {
           $("div.invoice-box-form").hide()
           $("div.invoice-box").empty()
           $("div.invoice-box").append(
