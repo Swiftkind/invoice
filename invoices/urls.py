e3aec182a9bef257903714f92527b20e34b68629
mich71095 <michaelpsy71095@gmail.com>
2018-02-26 16:58:48 +0800

    Change urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,24 +1,21 @@
 from django.urls import path
 from invoices.views import (
-    IndexView, 
-    InvoiceListView, 
-    InvoiceView, 
-    InvoiceAddView, 
-    InvoiceEditView, 
-    InvoiceDeleteView, 
-    InvoiceEmailView, 
-    PdfPreview, 
-    MakeInvoiceView,
+    IndexView,
     InvoiceAjaxView,
+    InvoiceView,
+    InvoiceDeleteView,
+    InvoiceEmailView,
+    MakeInvoiceView,
+    PdfPreview,
+    UpdateInvoiceForm,
     ) 
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('invoices/', InvoiceListView.as_view(), name='invoices'),
-    path('invoice/view/<int:invoice_id>/', InvoiceView.as_view(), name='invoice_view'),
-    path('invoice/add/', InvoiceAddView.as_view(), name='invoice_add'),
-    path('invoice/edit/<int:invoice_id>/', InvoiceEditView.as_view(), name='invoice_edit'),
+    path('invoices/', InvoiceView.as_view(), name='invoices'),
+    path('invoice/edit/<int:invoice_id>/<int:totalForm>/', UpdateInvoiceForm.as_view()
+                                                         , name='invoice_edit'),
     path('invoice/delete/<int:invoice_id>/', InvoiceDeleteView.as_view(), name='invoice_delete'),
     path('invoice/send_email/<int:invoice_id>/', InvoiceEmailView.as_view(), name='invoice_email'),
     path('invoice/pdf/<int:invoice_id>/', PdfPreview.as_view(), name='invoice_pdf'),
